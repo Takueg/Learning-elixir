93caf5cb7477f2ed9643b962b9d5a12609752be4
Takudzwa Gwindingwi <takueg@gmail.com>
2021-10-03 17:53:54 +0200

    created a function to welcome me to elixir

---
+++ programming-basics.ex
@@ -0,0 +1,20 @@
+defmodule Basics do
+  @moduledoc """
+  Defines functions to help learn elixir basics syntax and functions.
+  """
+  import IO, only: [gets: 1, puts: 1]
+
+  @doc """
+  Asks user for github username and prints out a personalised
+  welcome note to the elixir language.
+
+  ## Example
+
+    What is your github name? Takueg
+    Welcome to elixir Takueg
+  """
+  def welcome do
+    username = gets("What is your github name? ")
+    puts("Welcome to elixir #{username}")
+  end
+end
